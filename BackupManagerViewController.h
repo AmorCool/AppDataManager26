9951c161f1d28fd9229db331495284031977c430
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-08 17:53:30 +0300

    New UI: BackupManagerViewController header

--- BackupManagerViewController.h
+++ BackupManagerViewController.h
@@ -1,8 +1,4 @@
 #import <UIKit/UIKit.h>
 
 @interface BackupManagerViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
-
-- (instancetype)initWithBundleID:(NSString *)bundleID appName:(NSString *)appName;
-- (instancetype)init;
-
 @end
