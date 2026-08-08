255af65481dea02605905545402cd901cc5c80b6
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-08 12:00:14 +0300

    Add AppDelegate.m

---
+++ AppDelegate.m
@@ -0,0 +1,18 @@
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    navController.navigationBar.prefersLargeTitles = YES;
+
+    self.window.rootViewController = navController;
+    [self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+@end
