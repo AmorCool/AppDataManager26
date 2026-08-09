567b9866a6c95eaa8abe56132c13801168152064
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-09 05:50:00 +0300

    Update rootless.h

--- rootless.h
+++ rootless.h
@@ -1,23 +1,23 @@
 #ifndef ROOTLESS_H
 #define ROOTLESS_H
 
-#include <sys/syslimits.h>
+#include <sys/stat.h>
 #include <string.h>
 
 static inline const char *ROOT_PATH(const char *path) {
-    if (path[0] != '/') return path;
-    static char newPath[PATH_MAX];
-    strcpy(newPath, "/var/jb");
-    strcat(newPath, path);
-    return newPath;
+    if (access("/var/jb", F_OK) == 0) {
+        static char newPath[PATH_MAX];
+        snprintf(newPath, sizeof(newPath), "/var/jb%s", path);
+        return newPath;
+    }
+    return path;
 }
 
-#ifdef __OBJC__
-#import <Foundation/Foundation.h>
 static inline NSString *ROOT_PATH_NS(NSString *path) {
-    if (![path isAbsolutePath]) return path;
-    return [@"/var/jb" stringByAppendingPathComponent:path];
+    if (access("/var/jb", F_OK) == 0) {
+        return [@"/var/jb" stringByAppendingPathComponent:path];
+    }
+    return path;
 }
-#endif
 
 #endif
