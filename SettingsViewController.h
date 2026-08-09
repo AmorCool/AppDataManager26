1b4e6ff8206b18c9ece37f2fe118ecd0dbd5de56
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-09 14:05:42 +0300

    v1.3.0-fix: Add UITableView protocols + backupDirectory declaration

--- SettingsViewController.h
+++ SettingsViewController.h
@@ -1,4 +1,4 @@
 #import <UIKit/UIKit.h>
 
-@interface SettingsViewController : UIViewController
+@interface SettingsViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @end
