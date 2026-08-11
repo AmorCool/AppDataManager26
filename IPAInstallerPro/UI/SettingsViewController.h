cc05819b44ac42e6ee69ad84f183b0ce405b6d89
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-11 23:04:28 +0300

    Ensure SettingsViewController extends UITableViewController

--- IPAInstallerPro/UI/SettingsViewController.h
+++ IPAInstallerPro/UI/SettingsViewController.h
@@ -1,4 +1,4 @@
 #import <UIKit/UIKit.h>
 
-@interface SettingsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@interface SettingsViewController : UITableViewController
 @end
