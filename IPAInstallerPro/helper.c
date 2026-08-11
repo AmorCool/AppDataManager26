1dc7a7b59cf09dd587478d1e21872459d1e8af95
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-11 16:17:51 +0300

    FIX 13: Replace system() with posix_spawn() for iOS compatibility

--- IPAInstallerPro/helper.c
+++ IPAInstallerPro/helper.c
@@ -4,14 +4,17 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <spawn.h>
+
+extern char **environ;
 
 int main(int argc, char *argv[]) {
     if (setuid(0) != 0) {
-        fprintf(stderr, "ipainstallerpro-helper: setuid(0) failed: %s\n", strerror(errno));
+        fprintf(stderr, "ipainstallerpro_helper: setuid(0) failed: %s\n", strerror(errno));
         return 1;
     }
     if (setgid(0) != 0) {
-        fprintf(stderr, "ipainstallerpro-helper: setgid(0) failed: %s\n", strerror(errno));
+        fprintf(stderr, "ipainstallerpro_helper: setgid(0) failed: %s\n", strerror(errno));
         return 1;
     }
 
@@ -20,24 +23,14 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    size_t total_len = 0;
-    for (int i = 1; i < argc; i++) {
-        total_len += strlen(argv[i]) + 1;
-    }
-
-    char *cmd = malloc(total_len + 1);
-    if (!cmd) {
-        fprintf(stderr, "ipainstallerpro-helper: malloc failed\n");
+    pid_t pid;
+    int status = posix_spawn(&pid, argv[1], NULL, NULL, argv + 1, environ);
+    if (status != 0) {
+        fprintf(stderr, "ipainstallerpro_helper: posix_spawn failed: %s\n", strerror(status));
         return 1;
     }
 
-    cmd[0] = '\0';
-    for (int i = 1; i < argc; i++) {
-        if (i > 1) strcat(cmd, " ");
-        strcat(cmd, argv[i]);
-    }
-
-    int ret = system(cmd);
-    free(cmd);
-    return WIFEXITED(ret) ? WEXITSTATUS(ret) : ret;
+    int waitStatus;
+    waitpid(pid, &waitStatus, 0);
+    return WIFEXITED(waitStatus) ? WEXITSTATUS(waitStatus) : waitStatus;
 }
