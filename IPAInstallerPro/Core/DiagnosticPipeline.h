2f696b490c14d3716885aad22bb9bd1cd4638776
aosaid3224-ops <aosaid3224@gmail.com>
2026-08-13 01:20:46 +0300

    [P0-Fix] Fix import: CrashCorrelation.h -> CrashCorrelator.h

--- IPAInstallerPro/Core/DiagnosticPipeline.h
+++ IPAInstallerPro/Core/DiagnosticPipeline.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 #import "CrashIncident.h"
-#import "CrashCorrelation.h"
+#import "CrashCorrelator.h"
 
 @interface DiagnosticReport : NSObject
 @property (nonatomic, strong) NSDate *generatedAt;
